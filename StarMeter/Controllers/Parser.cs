8424219c1fce4dbb906dd1419752720e83a26d71
azpopov <azpopov@dundee.ac.uk>
2016-09-14 09:56:03 +0100

    more refactoring and tests written

--- StarMeter/Controllers/Parser.cs
+++ StarMeter/Controllers/Parser.cs
@@ -10,39 +10,44 @@ namespace StarMeter.Controllers
 {
     public class Parser
     {
-        private int portNumber;
+        public Recording recording;
 
         private const string DateTimeRegex = @"^(0[1-9]|1\d|2[0-8]|29(?=-\d\d-(?!1[01345789]00|2[1235679]00)\d\d(?:[02468][048]|[13579][26]))|30(?!-02)|31(?=-0[13578]|-1[02]))-(0[1-9]|1[0-2])-([12]\d{3}) ([01]\d|2[0-3]):([0-5]\d):([0-5]\d).(\d{3})$";
 
-        public void ParseFile()
+        public Recording ParseRecording(string path)
         {
+            recording = new Recording();
+
             //set up string buffer/etc.
-            var r = new StreamReader(@"C:\Users\arturpopov\Desktop\test2_link3.rec");
+            var r = new StreamReader(path);
+
+
+            recording.startStamp = ParseDateTime(r.ReadLine());
+            recording.portNumber = int.Parse(r.ReadLine());
+            r.ReadLine(); // Skip over whitespace
+
+            recording.packetList = ParsePackets(r, out recording.endStamp);
+
+            return recording;
+        }
+
+        public List<Packet> ParsePackets(StreamReader r, out DateTime recordingEndStamp)
+        {
             var line = "";
-
-            var startStamp = ParseDateTime(r.ReadLine());
-            DateTime endStamp;
-            portNumber = int.Parse(r.ReadLine());
-            r.ReadLine();
-
             var packetList = new List<Packet>();
-
-            DateTime date;
-           
             while ((line = r.ReadLine()) != null && r.Peek() > -1)
             {
                 var packet = new Packet();
-                
+
                 if (Regex.IsMatch(line, DateTimeRegex))
                 {
                     packet.DateRecieved = ParseDateTime(line);
                 }
-            
+
 
                 var packetType = r.ReadLine();
                 if (string.Compare(packetType, "P") == 0)
                 {
-                    
                     packet.Cargo = r.ReadLine().Split(' ');
                     var endingState = r.ReadLine();
                     packet.IsError = string.Compare(endingState, "EOP") != 0;
@@ -55,7 +60,8 @@ namespace StarMeter.Controllers
                 packetList.Add(packet);
                 r.ReadLine();
             }
-            endStamp = ParseDateTime(line);
+            recordingEndStamp = ParseDateTime(line);
+            return packetList;
         }
 
         public byte[] ParseCargo(string line)
