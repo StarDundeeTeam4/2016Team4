9569e98631f56bb91c9d067aa1b72af2d6be9a74
pratstercs <pratstercs@yahoo.co.uk>
2016-09-14 13:44:28 +0100

    Class to generate CRC values for a given packet. NB: CRC is generated from all other bytes in packet

---
+++ StarMeter.Tests/Controllers/CRCTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarMeter.Controllers;
+
+namespace StarMeter.Tests.Controllers
+{
+    [TestClass]
+    public class CRCTest
+    {
+
+        [TestMethod]
+        public void TestCRC()
+        {
+            byte[] packet = { 0x57, 0x01, 0x4c, 0x20, 0x2d, 0xff, 0xfb, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x08 };
+
+            ushort result = CRC.RMAP_CalculateCRC(packet);
+
+            Assert.AreEqual(result, 0x3e);
+            System.Diagnostics.Trace.WriteLine(CRC.ByteToHexString(result));
+        }
+    }
+}
