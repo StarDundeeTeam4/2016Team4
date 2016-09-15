97626ecae567e1b09597ff6fec7246da1bfebd0f
kimberleymccann <kimberleylmccann@gmail.com>
2016-09-15 10:58:45 +0100

    Added Analyser class with blank methods that need to be completed

---
+++ StarMeter/Controllers/Analyser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using StarMeter.Models;
+
+namespace StarMeter.Controllers
+{
+    public class Analyser
+    {
+
+        public int TotalNoOfDataChars(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+
+        public int TotalNoOfPackets(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+
+        public int TotalNoOfErrorPackets(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+
+        public int CalculateDataRate(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+
+        public int CalculatePacketRate(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+
+        public int CalculateErrorRate(Dictionary<Guid, Packet> packetDictionary)
+        {
+            //To Do
+            return 0;
+        }
+    }
+}
