c86a93d51d3ae8b9884d1a149376ff46b71d5fc5
kimberleymccann <kimberleylmccann@gmail.com>
2016-09-20 13:16:44 +0100

    Completed all error methods except disconnect

--- StarMeter/Controllers/ErrorDetector.cs
+++ StarMeter/Controllers/ErrorDetector.cs
@@ -6,38 +6,58 @@ namespace StarMeter.Controllers
 {
     public interface IErrorDetector
     {
-        ErrorTypes GetErrorType();
-        bool IsDataError();
-        bool IsSequenceError();
-        bool IsTimeoutError();
+        ErrorTypes GetErrorType(Packet previousPacket, Packet currentPacket);
+        bool IsDataError(Packet previousPacket, Packet currentPacket);
+        bool IsSequenceError(Packet previousPacket, Packet currentPacket);
+        bool IsTimeoutError(Packet previousPacket, Packet currentPacket);
         bool IsDisconnectError();
     }
 
     public class ErrorDetector : IErrorDetector
     {
-        public ErrorTypes GetErrorType()
+        public ErrorTypes GetErrorType(Packet previousPacket, Packet currentPacket)
         {
+            if (IsTimeoutError(previousPacket, currentPacket))
+            {
+                return ErrorTypes.Timeout;
+            }
+            if(IsDataError(previousPacket, currentPacket))
+            {
+                return ErrorTypes.DataError;
+            }
+            if (IsSequenceError(previousPacket, currentPacket))
+            {
+                return ErrorTypes.SequenceError;
+            }
             return ErrorTypes.None;
         }
 
-        public bool IsDataError()
+        public bool IsDataError(Packet previousPacket, Packet currentPacket)
         {
-            return false;
+            var isCrcCorrect = CRC.CheckCrcForPacket(currentPacket.FullPacket);
+            var isBabblingIdiot = CheckForBabblingIdiot(currentPacket, previousPacket);
+
+            return !isCrcCorrect || isBabblingIdiot;
         }
 
-        public bool IsSequenceError()
+        public bool IsSequenceError(Packet previousPacket, Packet currentPacket)
         {
-            return false;
+            return currentPacket.SequenceNum < previousPacket.SequenceNum;
         }
 
-        public bool IsTimeoutError()
+        public bool IsTimeoutError(Packet previousPacket, Packet currentPacket)
         {
-            return false;
+            return currentPacket.FullPacket.Length < previousPacket.FullPacket.Length;
         }
 
         public bool IsDisconnectError()
         {
             return false;
+        }
+
+        private static bool CheckForBabblingIdiot(Packet currentPacket, Packet previousPacket)
+        {
+            return previousPacket.FullPacket == currentPacket.FullPacket;
         }
     }
 }
