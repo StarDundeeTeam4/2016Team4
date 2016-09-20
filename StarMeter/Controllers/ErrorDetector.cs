3625d915f9af18bfda13e7d8ce98b883f1007390
kimberleymccann <kimberleylmccann@gmail.com>
2016-09-20 11:41:21 +0100

    Created empty methods for each of the error types

---
+++ StarMeter/Controllers/ErrorDetector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;   
+using StarMeter.Models;
+
+namespace StarMeter.Controllers
+{
+    public interface IErrorDetector
+    {
+        ErrorTypes GetErrorType();
+        bool IsDataError();
+        bool IsSequenceError();
+        bool IsTimeoutError();
+        bool IsDisconnectError();
+    }
+
+    public class ErrorDetector : IErrorDetector
+    {
+        public ErrorTypes GetErrorType()
+        {
+            return ErrorTypes.None;
+        }
+
+        public bool IsDataError()
+        {
+            return false;
+        }
+
+        public bool IsSequenceError()
+        {
+            return false;
+        }
+
+        public bool IsTimeoutError()
+        {
+            return false;
+        }
+
+        public bool IsDisconnectError()
+        {
+            return false;
+        }
+    }
+}
