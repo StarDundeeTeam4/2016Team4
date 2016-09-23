809a1058b06d485701a68948038e1954e45d2503
kimberleymccann <kimberleylmccann@gmail.com>
2016-09-23 12:15:08 +0100

    Added docs for methods

--- StarMeter/Controllers/ErrorDetector.cs
+++ StarMeter/Controllers/ErrorDetector.cs
@@ -13,6 +13,13 @@ namespace StarMeter.Controllers
 
     public class ErrorDetector : IErrorDetector
     {
+        /// <summary>
+        /// A method that gets the error type for an error packet 
+        /// by comparing it with the previous packet
+        /// </summary>
+        /// <param name="previousPacket">The previous packet from the current packet</param>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>An error type relating to the error that occured</returns>
         public ErrorType GetErrorType(Packet previousPacket, Packet currentPacket)
         {
             if (IsTimeoutError(previousPacket, currentPacket))
@@ -30,6 +37,13 @@ namespace StarMeter.Controllers
             return ErrorType.Disconnect;
         }
 
+        /// <summary>
+        /// Checks if the data is corrupt, causing a data error.
+        /// The can take the form of a babbling idiot or a CRC error
+        /// </summary>
+        /// <param name="previousPacket">The previous packet from the current packet</param>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>A bool indicating whether this error occurred, true if it did and false if it didn't</returns>
         public bool IsDataError(Packet previousPacket, Packet currentPacket)
         {
             var isCrcCorrect = IsCrcError(currentPacket);
@@ -38,21 +52,44 @@ namespace StarMeter.Controllers
             return isBabblingIdiot || !isCrcCorrect;
         }
 
+        /// <summary>
+        /// Checks if the sequence numbers are out of order, causing a sequence error.
+        /// </summary>
+        /// <param name="previousPacket">The previous packet from the current packet</param>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>A bool indicating whether this error occurred, true if it did and false if it didn't</returns>
         public bool IsSequenceError(Packet previousPacket, Packet currentPacket)
         {
             return currentPacket.SequenceNum < previousPacket.SequenceNum;
         }
 
+        /// <summary>
+        /// Checks if the transmission timedout and stopped sending packets, causing a timeout error.
+        /// </summary>
+        /// <param name="previousPacket">The previous packet from the current packet</param>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>A bool indicating whether this error occurred, true if it did and false if it didn't</returns>
         public bool IsTimeoutError(Packet previousPacket, Packet currentPacket)
         {
             return currentPacket.FullPacket.Length < previousPacket.FullPacket.Length;
         }
 
-        private static bool CheckForBabblingIdiot(Packet currentPacket, Packet previousPacket)
+        /// <summary>
+        /// Checks if the transmission is repeatedly sending the same packet.
+        /// </summary>
+        /// <param name="previousPacket">The previous packet from the current packet</param>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>A bool indicating whether this error occurred, true if it did and false if it didn't</returns>
+        private static bool CheckForBabblingIdiot(Packet previousPacket, Packet currentPacket)
         {
             return previousPacket.FullPacket.SequenceEqual(currentPacket.FullPacket);
         }
 
+        /// <summary>
+        /// Checks if the CRC value of the packet is the correct value
+        /// </summary>
+        /// <param name="currentPacket">The current packet being checked for error</param>
+        /// <returns>A bool indicating whether the value is correct, true if it if and false if it isn't</returns>
         private static bool IsCrcError(Packet currentPacket)
         {
             bool crcValid;
